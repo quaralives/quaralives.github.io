df0baba56bed12f3775ef7f0db776f1a02c7cc90
Diogo Maximo <diogomaximo@MacBook-Air-de-Diogo.local>
2020-04-24 20:51:54 -0300

    initial

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,19 +3,19 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "index.html": "1a6155a5250d8e827b8ceef46715c838",
 "/": "1a6155a5250d8e827b8ceef46715c838",
-"main.dart.js": "30147f62fa5a96b541502435444092d8",
+"main.dart.js": "1d3a851e64217f1614b22ce3a985869c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "32a3b0c13ccc2fd6d6645fefe0a28714",
-"assets/LICENSE": "6f91ad674328670160bc955ba992ccf9",
+"assets/LICENSE": "c5d6093206de995baa02518ae741b88d",
 "assets/AssetManifest.json": "2c33796f95111d497f2560eff5ca2d53",
 "assets/FontManifest.json": "92f8fe616884b539a8c9a6b2ae33b130",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "assets/packages/line_awesome_icons/assets/fonts/icon_font.ttf": "4d42f5f0c62a8f51e876c14575354a6e",
 "assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
 "assets/assets/loading.gif": "262a150360ab4ff3379879f702f99617",
-"assets/assets/lives.json": "b4daa9c1a3000389ad6b7f7cf7df258f"
+"assets/assets/lives.json": "b432a071014842205d06435a3b03b6f8"
 };
 
 self.addEventListener('activate', function (event) {
